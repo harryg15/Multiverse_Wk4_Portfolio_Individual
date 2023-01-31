9681c8554fb6ed7c57ff2c15dc72031609e37a80
harryg15 <harry.grant99@googlemail.com>
2023-01-31 10:18:19 +0000

    added animations on hover for navbar (responsive)

--- animation.js
+++ animation.js
@@ -2,7 +2,8 @@ let home = bodymovin.loadAnimation({
     container: document.getElementById('home'),
     path: "assets/home.json",
     render: "svg",
-    loop: true,
+    loop: false,
+    autoplay: false,
     name: "home"
 })
 
@@ -10,21 +11,64 @@ let bio = bodymovin.loadAnimation({
     container: document.getElementById('bio'),
     path: "assets/avatar.json",
     render: "svg",
-    loop: true,
+    loop: false,
+    autoplay: false,
     name: "bio"
 })
 let cv = bodymovin.loadAnimation({
     container: document.getElementById('cv'),
     path: "assets/cv.json",
     render: "svg",
-    loop: true,
+    loop: false,
+    autoplay: false,
     name: "cv"
 })
 let portfolio = bodymovin.loadAnimation({
     container: document.getElementById('portfolio'),
     path: "assets/responsive.json",
     render: "svg",
-    loop: true,
+    loop: false,
+    autoplay: false,
     name: "portfolio"
 })
+
+const homeTag = document.getElementById('home')
+homeTag.addEventListener('mouseenter', () => {
+    home.play()
+} )
+homeTag.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        home.stop()
+    }, 1500)
+} )
+
+const bioTag = document.getElementById('bio')
+bioTag.addEventListener('mouseenter', () => {
+    bio.play()
+} )
+bioTag.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        bio.stop()
+    }, 1500)
+} )
+
+const cvTag = document.getElementById('cv')
+cvTag.addEventListener('mouseenter', () => {
+    cv.play()
+} )
+cvTag.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        cv.stop()
+    }, 1500)
+} )
+
+const portfolioTag = document.getElementById('portfolio')
+portfolioTag.addEventListener('mouseenter', () => {
+    portfolio.play()
+} )
+portfolioTag.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        portfolio.stop()
+    }, 2000)
+} )
 
